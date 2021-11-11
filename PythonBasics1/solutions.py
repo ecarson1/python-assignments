6a681df004d909371776a5c3c00abeefcf940454
ecarson1 <ethan.carson@smoothstack.com>
2021-11-10 20:13:36 -0500

    Push day one assignments

---
+++ PythonBasics1/solutions.py
@@ -0,0 +1,20 @@
+# Question 1 Solution
+sum = 50 + 50
+diff = 100 - 10
+print(sum)
+print(diff)
+
+# Question 2 Solution
+# I was confused as to what this question was asking
+print(30,6^6,6**6,6+6+6+6+6+6)
+
+# Question 3 Solution
+print("Hello World")
+print("Hello World: 10")
+
+# Question 4 Solution
+def get_m(p, r, l):
+    comp = pow((1 + r/(12 * 100)), l)
+    return (p * comp * (r/1200))/(comp-1)
+
+print(get_m(800000, 6, 103))
